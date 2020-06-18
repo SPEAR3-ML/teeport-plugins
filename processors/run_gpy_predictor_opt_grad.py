6e62ea6efc8aa493fbc049bce3f65ffc8ecdd6f3
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-06-18 14:15:32 -0700

    Add the GPy predictor with gradient info

---
+++ processors/run_gpy_predictor_opt_grad.py
@@ -0,0 +1,15 @@
+import numpy as np
+import nest_asyncio
+nest_asyncio.apply()
+import asyncio
+from functools import partial
+
+from teeport import Teeport
+from opt.processors.gpy_predictor import process
+
+teeport = Teeport('ws://lambda-sp3:8090/')
+teeport.run_processor(partial(process, params={'ret_grad': True}), 'GPy Predictor with Grad')
+print('processor is running...')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(teeport.processors[0].task)
