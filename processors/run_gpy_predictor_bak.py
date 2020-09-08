3538339c0830d57a6fc9d734775890554e776104
Zhe Zhang <wenatuhs@gmail.com>
2020-09-08 00:13:31 -0700

    Back up the old GPy preditor runner

---
+++ processors/run_gpy_predictor_bak.py
@@ -0,0 +1,17 @@
+import numpy as np
+import nest_asyncio
+nest_asyncio.apply()
+import asyncio
+from functools import partial
+
+from teeport import Teeport
+from opt.processors.gpy_predictor import process
+
+params = dict(theta=np.array([[0.4]]), var='auto', sigma_n=np.array([[1e-4]]))
+
+teeport = Teeport('ws://lambda-sp3:8090/')
+teeport.run_processor(partial(process, params=params), 'GPy Predictor: no opt')
+print('processor is running...')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(teeport.processors[0].task)
