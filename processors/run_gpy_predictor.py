33f6a3c799a8995062be4f567d5f54537d87c435
Zhe Zhang <wenatuhs@gmail.com>
2020-09-08 00:14:47 -0700

    Combine all the old GPy predictor runners into one processor runner

--- processors/run_gpy_predictor.py
+++ processors/run_gpy_predictor.py
@@ -1,16 +1,17 @@
-import numpy as np
 import nest_asyncio
 nest_asyncio.apply()
 import asyncio
-from functools import partial
 
 from teeport import Teeport
 from opt.processors.gpy_predictor import process
 
-params = dict(theta=np.array([[0.4]]), var='auto', sigma_n=np.array([[1e-4]]))
-
 teeport = Teeport('ws://lambda-sp3:8090/')
-teeport.run_processor(partial(process, params=params), 'GPy Predictor: no opt')
+teeport.run_processor(process, 'GPy-Pred', name='GPy Predictor', configs={
+    'theta': None,
+    'var': None,
+    'sigma_n': None,
+    'ret_grad': False
+})
 print('processor is running...')
 
 loop = asyncio.get_event_loop()
